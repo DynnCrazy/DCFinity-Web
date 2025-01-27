aa37a067e25c8c60c57f1b10613484d2bbd4c47d
Dyn Crazy <121717799+DynnCrazy@users.noreply.github.com>
2025-01-27 14:34:57 +0800

    Add files via upload

--- Ast/Script/display.js
+++ Ast/Script/display.js
@@ -1,5 +1,5 @@
 const botName = "DCFinity"
-const botDesc = "DCFinity adalah bot seru berbahasa Indonesia yang dimana bisa membuat kamu bahagia dan tertawa tanpa batas (infinity). Bot ini memiliki perintah seperti fun & minigames."
+const botDesc = "DCFinity adalah bot seru berbahasa Indonesia yang dimana bisa membuat kamu bahagia dan tertawa tanpa batas (infinity). Bot ini memiliki perintah seperti fun, minigames, & utility."
 const thisYear = "2025"
 const botProfil = "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiEZXuIUuF8pySnOnPCQP6MQY9fy-QUYTm-5nJnun6QFYxRMFR_pzKbqzR7sAHZGHVmw9fhNKea11PAZCBKS9W94iYOhxOEb0OocY2QnHTizrHcSOgili8p46OKAvTudbmQaObLNNmyPjNTrRW9OjBIWX_98X7wgtH0JTxfTgtvqJfC9BoHiqlMcLxJDA/s3264/1000085530.png"
 
